a912fca7c3bb91c858f2e16c6e9d935421eb94fc
Cameron <cameronkwalden@gmail.com>
2021-11-17 15:40:49 -0800

    removes console logs from testing

--- src/middleware/basic.js
+++ src/middleware/basic.js
@@ -7,14 +7,9 @@ module.exports = async (req, res, next) => {
   if (!req.headers.authorization) {
     next('No auth header');
   }
-
-  console.log(req.headers.authorization);
-
   const basicHeaderParts = req.headers.authorization.split(' ');
   const encodedString = basicHeaderParts.pop();
   const decodedString = base64.decode(encodedString);
-  console.log(decodedString);
-
   const [username, password] = decodedString.split(':');
 
   try {
