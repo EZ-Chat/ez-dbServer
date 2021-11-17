169a432093739b4890217747a36659de08e8ec18
Evan Woodworth <evan.woodworth@gmail.com>
2021-11-16 16:32:58 -0800

    add friend

--- src/router.js
+++ src/router.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 const basicAuth = require('./middleware/basic.js');
 const bearerAuth = require('./middleware/bearer.js');
-const { signup, signin, update, remove } = require('./models/routes.js');
+const { signup, signin, update, addFriend, remove } = require('./models/routes.js');
 
 router.post('/signup', signup);
 router.post('/signin', basicAuth, signin);
+router.put('/addFriend/:id', bearerAuth, addFriend);
 router.put('/user/:id', bearerAuth, update);
 router.delete('/user/:id', bearerAuth, remove);
 
